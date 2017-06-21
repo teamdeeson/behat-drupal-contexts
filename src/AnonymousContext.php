3ff3d3186d90e4a5d9dd592dcb5e95a44048a554
Chris Jansen <chrisj@deeson.co.uk>
2017-06-21 22:32:10 +0100

    Extend anonymousContext with additional assertions

--- src/AnonymousContext.php
+++ src/AnonymousContext.php
@@ -4,6 +4,7 @@ namespace TeamDeeson\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Mink\Exception\ElementTextException;
 use Drupal\DrupalExtension\Context\MinkContext;
 
 /**
@@ -27,6 +28,53 @@ class AnonymousContext extends MinkContext implements Context, SnippetAcceptingC
     if (empty($result)) {
       throw new \Exception(sprintf('No class "%s" in the "%s" region on the page %s', $selector, $regionName, $this->getSession()->getCurrentUrl()));
     }
+  }
+
+  /**
+   * Finds and navigates to content of a given type with a given title.
+   *
+   * @When I am viewing a/an existing :type :entity_type with the title :title
+   */
+  public function viewNode($type, $title, $entity_type) {
+    $entityStorage = \Drupal::entityTypeManager()->getStorage($entity_type);
+    $query = $entityStorage->getQuery();
+
+    $entityType = $entityStorage->getEntityType();
+    $results = $query->condition($entityType->getKey('bundle'), $type)
+      ->condition($entityType->getKey('label'), $title)
+      ->execute();
+
+    if (empty($results)) {
+      throw new \Exception("No {$type} with the title '{$title}' could be found");
+    }
+
+    $entity = $entityStorage->load(end($results));
+    $url = $entity->toUrl();
+    $path = $url->getInternalPath();
+    $this->assertAtPath($url->toString());
+  }
+
+  /**
+   * Checks, that page contains specified text within a parent element with a
+   * given css selector.
+   * Example: Then I should see "Who is the Batman?" rendered as "block"
+   * Example: And I should see "Who is the Batman?" rendered as "block"
+   *
+   * @Then /^(?:|I )should see "(?P<text>(?:[^"]|\\")*)" rendered as (?P<selector>(?:[^"]|\\")*)$/
+   */
+  public function assertAtLeastOneElementContainsText($text, $selector) {
+    /** @var NodeElement[] $items */
+    $items = $this->getSession()->getPage()->findAll('css', $selector);
+
+    /** @var \Behat\Mink\Element\NodeElement $item */
+    while ($item = array_shift($items)) {
+      $regex = '/'.preg_quote($text, '/').'/ui';
+      if (preg_match($regex, $item->getText())) {
+        return;
+      }
+    }
+
+    throw new \Exception("No {$selector} element containing '{$text}' could be found.'");
   }
 
 }
